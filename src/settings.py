48d1dd968488a423be4103f23db6d15801a918df
Caspar Kroll <mail@casparkroll.de>
2026-07-12 22:43:35 +0200

    Adjusted scrapy settings for more concurrency

--- src/settings.py
+++ src/settings.py
@@ -4,11 +4,15 @@ SPIDER_MODULES = ["src.parser"]
 NEWSPIDER_MODULE = "src.parser"
 
 # Be polite: identify ourselves and throttle requests
-USER_AGENT = "AlmaWebParser/1.0 (+https://github.com/almaweb-parser)"
+USER_AGENT = "AlmaWebParser/1.0 (+https://github.com/natureFrameworkManager/almaweb-parser)"
+BOT_NAME = "almaweb_lecture_parser"
 ROBOTSTXT_OBEY = True
 
 # Throttle to avoid overloading the server
 AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_TARGET_CONCURRENCY = 16
+AUTOTHROTTLE_START_DELAY = 1.0
+AUTOTHROTTLE_MAX_DELAY = 10.0
 # DOWNLOAD_DELAY = 1
 # CONCURRENT_REQUESTS = 10
 # CONCURRENT_REQUESTS_PER_DOMAIN = 10
@@ -28,6 +32,9 @@ TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 FEED_EXPORT_ENCODING = "utf-8"
+
+DNS_TIMEOUT = 5
+DOWNLOAD_TIMEOUT = 15
 
 # Limit crawled pages
 # CLOSESPIDER_PAGECOUNT = 20  # Stop after 10 responses
